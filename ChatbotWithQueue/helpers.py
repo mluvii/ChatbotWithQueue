c25acd01f264e1190ec92f247f9f6f6dcaa43411
Jan N <jan.novopacky@gmail.com>
2023-03-21 17:24:51 +0100

    WorkerData class changes

--- ChatbotWithQueue/helpers.py
+++ ChatbotWithQueue/helpers.py
@@ -2,19 +2,17 @@ from datetime import datetime, timezone
 from dataclasses import dataclass
 
 
-class WorkerData:
-    def __init__(self, server, bot, data):
-        self.bot = bot
-        self.data = data
-        self.server = server
-
-
 @dataclass(frozen=True)
 class Bot:
     chatbot_id: str
     client_id: str
     client_secret: str
 
+@dataclass
+class WorkerData:
+    server:str
+    bot: Bot
+    data:object
 
 def datetime_now_iso():
     return datetime.now(timezone.utc).astimezone().isoformat()
